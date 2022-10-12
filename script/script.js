9b75252219573b1f4aa8faa852740afc54e58200
Alla Berkana <allaberkana@gmail.com>
2022-10-12 17:53:37 +0300

    lesson-2

--- script/script.js
+++ script/script.js
@@ -1,3 +1,14 @@
+// форматирование сумм: разделитель нулей, значок валюты
+const formatCurrency = (num) => {
+    const currency = new Intl.NumberFormat('ru-RU', {
+        style: 'currency',
+        currency: 'RUB',
+        maximumFractionDigits: 2,
+    });
+    return currency.format(num);
+};
+
+
 const navigationLinks = document.querySelectorAll('.navigation__link');
 const calcElems = document.querySelectorAll('.calc');
 
@@ -47,13 +58,13 @@ calcLabelExpenses.style.display = 'none';
 formAusn.addEventListener('input', () => {
     if (formAusn.type.value === 'income') {
         calcLabelExpenses.style.display = 'none';
-        resultTaxTotal.textContent = formAusn.income.value * 0.08;
+        resultTaxTotal.textContent = formatCurrency(formAusn.income.value * 0.08);
         formAusn.expenses.value = '';
     }
     if (formAusn.type.value === 'expenses') {
         calcLabelExpenses.style.display = ''; //декларативно возвращаем стили
-        resultTaxTotal.textContent =
-            (formAusn.income.value - formAusn.expenses.value) * 0.2;
+        resultTaxTotal.textContent = formatCurrency(
+            (formAusn.income.value - formAusn.expenses.value) * 0.2);
     }
 });
 
@@ -61,9 +72,52 @@ formAusn.addEventListener('input', () => {
 const selfEmployment = document.querySelector('.self-employment');
 const formSelf = selfEmployment.querySelector('.calc__form');
 const resultTaxTotalSelf = selfEmployment.querySelector('.result__tax_total');
+const calcCompensation = selfEmployment.querySelector('.calc__label_compensation');
+const resultBlockCompensation = selfEmployment.querySelectorAll('.result__block_compensation');
+
+const resultTaxCompensation = selfEmployment.querySelector('.result__tax_compensation');
+const resultTaxRestCompensation = selfEmployment.querySelector('.result__tax_rest-compensation');
+const resultTaxResult = selfEmployment.querySelector('.result__tax_result');
+
+const checkCompensation = () => {
+    const setDisplay = formSelf.addCompensation.checked ? '' : 'none';
+    calcCompensation.style.display = setDisplay;
+    resultBlockCompensation.forEach(item => item.style.display = setDisplay);
+    // if (formSelf.addCompensation.checked === true) {
+    //     calcCompensation.style.display = '';
+    // } else {
+    //     calcCompensation.style.display = 'none';
+    // }
+};
+
+checkCompensation();
+// calcCompensation.style.display = 'none';
 
 formSelf.addEventListener('input', () => {
     const personTaxSelf = formSelf.person.value * 0.04;
     const entityTaxSelf = formSelf.entity.value * 0.06;
-    resultTaxTotalSelf.textContent = personTaxSelf + entityTaxSelf;
+    checkCompensation();
+
+    const tax = personTaxSelf + entityTaxSelf
+    // проверка на 10 000
+    formSelf.compensation.value =
+        formSelf.compensation.value > 10_000 ?
+            10_000 :
+            formSelf.compensation.value;
+
+    const benefit = formSelf.compensation.value;
+    const resultBenefit = formSelf.person.value * 0.01 +
+        formSelf.entity.value * 0.02;
+    const finalBenefit = (benefit - resultBenefit) > 0 ?
+        (benefit - resultBenefit) : 0;
+    const finalTax = tax - (benefit - finalBenefit);
+
+
+    resultTaxTotalSelf.textContent = formatCurrency(tax);
+    resultTaxCompensation.textContent = formatCurrency(benefit - finalBenefit);
+    resultTaxRestCompensation.textContent = formatCurrency(finalBenefit);
+    resultTaxResult.textContent = formatCurrency(finalTax);
+
 })
+
+
